ff502ee800962874a18d19e5f62780f2ea13056d
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-11-01 20:46:43 -0600

    changed time delay between measurements to 0.5s

--- SensorTest.py
+++ SensorTest.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+            time.sleep(.5)
 
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
