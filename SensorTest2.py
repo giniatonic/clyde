eae8d7306f6dd056cad81f7964508b4905db1a3b
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-11-07 20:56:04 -0700

    trying low trigger

--- SensorTest2.py
+++ SensorTest2.py
@@ -16,6 +16,9 @@ trigPin = 23
 io.setup(senPin, io.IN)
 io.setup(trigPin, io.OUT)
 
+#set to high to begin with
+io.output(trigPin, True)
+
 #variables
 #int senValue = 0
 #int distValue = 0
@@ -23,9 +26,9 @@ io.setup(trigPin, io.OUT)
 time.sleep(0.5)
 
 def distMeasure():
-    io.output(trigPin, True)
+    io.output(trigPin, False) #low trigger
     time.sleep(0.00001)
-    io.output(trigPin, False)
+    io.output(trigPin, True)
     senValue = io.input(senPin)
     distMeasure = senValue*0.718
     return distMeasure
