ff56929269e07cdcaa98cb3cdb30fa20eaa4176a
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-11-07 21:41:50 -0700

    debug

--- SensorTest.py
+++ SensorTest.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(.5o y)
+            time.sleep(.5)
 
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
