3565a84a90066f2c12fe957363f177602d2b63fc
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-11-10 18:35:19 -0700

    stuff

--- SensorTest.py
+++ SensorTest.py
@@ -10,7 +10,7 @@ GPIO_TRIGGER = 18
 GPIO_ECHO = 24
 
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 def distance():
