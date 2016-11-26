8c7aeedb92c1bd34d075a1ef5f3ae0bd5b34beb1
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-11-25 21:09:01 -0700

    trying a thing

--- clyde3.py
+++ clyde3.py
@@ -28,15 +28,13 @@ def measure(trigpin,echopin):
     GPIO.output(trigpin, False)
     start = time.time()
 
-
     while GPIO.input(echopin)==0:
         start = time.time()
 
-    #stop = time.time()
+    stop = time.time()
 
     while GPIO.input(echopin)==1:
         stop = time.time()
-        #print('blah')
 
     elapsed = stop-start
     distance = (elapsed * 34300)/2
