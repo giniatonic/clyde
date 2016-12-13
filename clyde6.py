fdc02164fb82a179a619db5af192bc13637f00c0
Virginia Schilling <virginiaschilling@VirginilingsMBP.PK5001Z>
2016-12-13 10:47:13 -0700

    editing logic

--- clyde6.py
+++ clyde6.py
@@ -126,6 +126,11 @@ if __name__ == '__main__':
             if stopped == 0
                 if(dF > 40 and dR < 35 and dL < 35):
                 #In a corridor
+                    if(abs(dL-dR) > 5):
+                        if(dL > dR):
+                            clyde.left(100,1)
+                        else:
+                            clyde.right(100,1)
                     clyde.forward(50) #go forward
                     stopped = 0
                 elif(dF < 40):
@@ -147,8 +152,11 @@ if __name__ == '__main__':
                         clyde.left(50)
                 elif(dR<35 and dL<35):
                 #Cornered!
-                    clyde.reverse()
-                    clyde.left(50)
+                    clyde.backward(30,2)
+                    clyde.left(100,10)
+                elif(dF>40 and dR<35 and dL<35)
+                    clyde.forward(50)
+                    stopped = 0
 
             time.sleep(.1)
             #print('distances: %.1f , %.1f' % (distances[0], distances[1]))
